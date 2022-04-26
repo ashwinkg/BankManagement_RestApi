08e242e82fd78104826419e03233947aa4258820
Ashwin Kadammaje Giridhar <ashu.ashwinkg@gmail.com>
2022-04-26 01:06:36 -0500

    Intial push into the repo

---
+++ src/main/java/com/ashwin/bankmgmt_rest_api/BankManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ashwin.bankmgmt_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashwin/bankmgmt_rest_api/BankManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashwin.bankmgmt_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
